f3a20c272ebafea6a4db64abd8a43411e9a1e008
JoseluRuiz <joselurb2005@gmail.com>
2024-10-28 13:59:12 +0100

    TRANSICIONES Y PANTALLA DE CARGA

---
+++ app/src/main/java/com/example/interfaces/Splash.java
@@ -0,0 +1,63 @@
+package com.example.interfaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        openApp();
+
+
+        ImageView thunder = findViewById(R.id.logosplash);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.blink);
+        thunder.startAnimation(anim);
+
+
+        ImageView mSea = findViewById(R.id.fondo);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1592807749311-c1d79ff9082f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.black)))
+                .into(mSea);
+
+    }
+
+    private void openApp() {
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash
+                        .this, Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+            }
+        }, 5000);
+
+
+    }
+
+}
