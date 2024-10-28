cd619193430b1ec938ba2492fa91e2d6619ed0b8
JoseluRuiz <joselurb2005@gmail.com>
2024-10-28 12:28:25 +0100

    CAMBIOS

--- app/src/main/java/com/example/interfaces/Main.java
+++ app/src/main/java/com/example/interfaces/Main.java
@@ -1,12 +1,17 @@
 package com.example.interfaces;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Main extends AppCompatActivity {
 
@@ -15,6 +20,16 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ImageView mGirl = findViewById(R.id.foto);
+
+        Glide.with(this)
+                .load(R.drawable.hipman)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .circleCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.black)))
+                .into(mGirl);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
