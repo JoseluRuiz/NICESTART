2ca6c25d2fd233928ae6865a24b65e49b8187336
JoseluRuiz <joselurb2005@gmail.com>
2024-11-28 11:49:25 +0100

    Boton Profile (para ver cambios con colaboradores)

--- app/src/main/java/com/example/interfaces/Profile.java
+++ app/src/main/java/com/example/interfaces/Profile.java
@@ -1,6 +1,9 @@
 package com.example.interfaces;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Profile extends AppCompatActivity {
 
+    Button b;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+
+        b = findViewById(R.id.botonprf);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this, Main.class);
+                startActivity(intent);
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
