84dd69a991821f882d4ccc566bc04a475e59cbc4
JoseluRuiz <joselurb2005@gmail.com>
2025-01-22 12:37:16 +0100

    FAB

---
+++ app/src/main/java/com/example/interfaces/mainbab.java
@@ -0,0 +1,80 @@
+package com.example.interfaces;
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class mainbab extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainbab);
+
+//        this.getSupportActionBar().setDisplayShowCustomEnabled(true);
+//
+//        LayoutInflater inflater = LayoutInflater.from(this);
+//        View v = inflater.inflate(R.layout.custom_action_bar, null);
+//
+//        TextView titleTextView = (TextView) v.findViewById(R.id.custom_action_bar_title);
+//        titleTextView.setText(this.getTitle());
+//        titleTextView.setTypeface(App.getInstance().getActionBarTypeFace());
+//
+//        this.getSupportActionBar().setCustomView(v);
+
+        // cast al xml
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
+        FloatingActionButton myfab = findViewById(R.id.fab);
+
+        //click event en el  FAB
+        myfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mainbab.this, "FAB Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //click event en el Hamburguer menu
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mainbab.this, "Menu clicked", Toast.LENGTH_SHORT).show();
+//                sheetBehavior = BottomSheetBehavior.from(sheet);
+            }
+
+
+        });
+
+        //click event en el Bottom bar menu item
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                int id = item.getItemId();
+
+                if(id == R.id.heart)
+                {
+                    Toast.makeText(mainbab.this, "Added to favourites", Toast.LENGTH_SHORT).show();
+                }
+                if(id == R.id.search)
+                {
+                    Toast.makeText(mainbab.this, "Beginning search", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
+
+    }
+
+}
